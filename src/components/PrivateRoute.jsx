752d1a74700114a803d515962c6829155663623f
Mahmudul <mahmudul@blubnc.com>
2025-01-24 13:12:13 +0600

    loader  updated

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,11 +1,12 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import Loader from './loader/Loader';
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div><Loader/></div>;
   }
 
   return user ? children : <Navigate to="/login" />;
